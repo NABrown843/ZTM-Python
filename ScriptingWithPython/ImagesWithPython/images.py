48d08ee85b0ac36225fdb019c07a217e500d1a5a
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-01-30 01:07:16 -0500

    Section 17 - Scripting with Python Coursework

---
+++ ScriptingWithPython/ImagesWithPython/images.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageFilter
+
+# img = Image.open('.\\Pokedex\\pikachu.jpg')
+# # # filtered_img = img.filter(ImageFilter.BLUR)
+# # converted_img = img.convert('L')
+# # resize = converted_img.resize((300,300))
+# box = (100, 100, 400, 400)
+# region = img.crop(box)
+# region.save('cropped_img.png', 'png')
+# region.show()
+
+# print(img)
+# print(img.format)
+# print(img.size)
+# print(img.mode)
+# resize.save('resized_img.png','png')
+# resize.show()
+
+img = Image.open('astro.jpg')
+img.thumbnail((400,400))
+img.save('astro_thumbnail.jpg')
+img.show()
